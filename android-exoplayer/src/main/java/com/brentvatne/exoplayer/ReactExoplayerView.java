74b6069ad8aaa84e57c27f84b8065e72651ffff3
Simone Carcone <simonecarcone@gmail.com>
2023-10-13 10:09:48 +0200

    Update ReactExoplayerView.java
    fix onAttachedWindow

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -234,7 +234,9 @@ class ReactExoplayerView extends FrameLayout implements
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        initializePlayer();
+        if(player == null){
+            initializePlayer();
+        }
     }
 
     @Override
